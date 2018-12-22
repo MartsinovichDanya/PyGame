70d61d97a59acfc20e2b93a609a25c4f0702f48d
MartsinovichDanya <martsinovich739@gmail.com>
2018-12-22 17:48:59 +0300

    Update shahmotnay_kletka.py

--- shahmotnay_kletka.py
+++ shahmotnay_kletka.py
@@ -9,9 +9,9 @@ def draw_rect(n):
         x = 0
         for j in range(n):
             if not (i + j) % 2:
-                pygame.draw.rect(screen, (255, 255, 255), (x, y, w, h))
-            else:
                 pygame.draw.rect(screen, (0, 0, 0), (x, y, w, h))
+            else:
+                pygame.draw.rect(screen, (255, 255, 255), (x, y, w, h))
             x += w
         y += h
 
