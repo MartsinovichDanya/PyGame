8eb520ebe09e6a98c0e04c4e08912294c4a620ff
Danya <den.martsinovich@yandex.ru>
2019-01-02 17:26:21 +0300

    ventilator

--- ventilator.py
+++ ventilator.py
@@ -2,42 +2,41 @@ import pygame
 import math
 
 
-class Blade:
-    def __init__(self, p1, p2, p3):
-        self.p1 = p1
-        self.p2 = p2
-        self.p3 = p3
-
-    def draw(self):
-        pygame.draw.polygon(screen, pygame.Color('white'), [self.p1, self.p2, self.p3])
-
-    def turn(self, angle):
-        x2 = self.p1[0] + int(70 * math.cos(math.radians(angle)))
-        y2 = self.p1[1] + int(70 * math.sin(math.radians(angle)))
-        return x2, y2
+def turn(alpha, step, s):
+    radius = 67
+    centre = 100
+    shag_v_rad = math.pi / 12
+    pygame.draw.polygon(s, pygame.Color('white'),
+                        [(centre, centre),
+                         (int(centre + radius * math.sin(alpha + shag_v_rad * step)),
+                          int(centre + radius * math.cos(alpha + shag_v_rad * step))),
+                         (int(centre + radius * math.sin(alpha + shag_v_rad * (step + 2))),
+                          int(centre + radius * math.cos(alpha + shag_v_rad * (step + 2))))])
 
 
 pygame.init()
 size = width, height = 201, 201
 screen = pygame.display.set_mode(size)
-pygame.draw.circle(screen, pygame.Color('white'), (101, 101), 10)
-blade1 = Blade((101, 101), (118, 33), (84, 33))
-blade1.draw()
-blade2 = Blade((101, 101), blade1.turn(15), blade1.turn(45))
-blade2.draw()
-blade3 = Blade((101, 101), blade1.turn(135), blade1.turn(165))
-blade3.draw()
-pygame.display.flip()
 clock = pygame.time.Clock()
 running = True
+v = 0
+alpha = 0
 while running:
     tick = clock.tick(30)
+    screen.fill((0, 0, 0))
+    pygame.draw.circle(screen, pygame.Color('white'), (101, 101), 10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                pass
-    # screen.fill((0, 0, 0))
-
+                v += math.pi / 90
+            elif event.button == 3:
+                v -= math.pi / 90
+    alpha += v
+    if alpha > 6.3:
+        alpha = 0
+    turn(alpha, 3, screen)
+    turn(alpha, 11, screen)
+    turn(alpha, 19, screen)
     pygame.display.flip()
