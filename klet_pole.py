9e0c0ca68a10e1c08c74c2291292aebd6e1cc1d9
Danya <den.martsinovich@yandex.ru>
2019-01-06 18:02:02 +0300

    klet_pole

--- klet_pole.py
+++ klet_pole.py
@@ -18,9 +18,28 @@ class Board:
         self.top = top
         self.cell_size = cell_size
 
+    def get_cell(self, mouse_pos):
+        x, y = mouse_pos
+        if (x < self.left or x > self.left + self.cell_size * self.width) or \
+           (y < self.top or y > self.top + self.cell_size * self.height):
+            return None
+        else:
+            res_x = (x - self.left) // self.cell_size
+            res_y = (y - self.top) // self.cell_size
+            return res_x, res_y
+
+    def on_click(self, cell_coords):
+        x, y = cell_coords
+        for i in range(self.height):
+            for j in range(self.width):
+                if i == y or j == x:
+                    self.board[i][j] = int(not self.board[i][j])
+
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
-        self.on_click(self, cell)
+        print(cell)
+        if cell:
+            self.on_click(cell)
 
     def render(self):
         for i in range(self.height):
@@ -29,19 +48,24 @@ class Board:
                         self.top + self.cell_size * i,
                         self.cell_size,
                         self.cell_size)
-                pygame.draw.rect(screen, (255, 255, 255), rect, 1)
+                if self.board[i][j]:
+                    pygame.draw.rect(screen, (255, 255, 255), rect)
+                else:
+                    pygame.draw.rect(screen, (255, 255, 255), rect, 1)
 
 
 pygame.init()
 size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
-board = Board(4, 3)
-board.set_view(100, 100, 50)
+board = Board(8, 9)
+board.set_view(50, 50, 50)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            board.get_click(event.pos)
     screen.fill((0, 0, 0))
     board.render()
     pygame.display.flip()
