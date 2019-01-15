5716448812d03532bae554782133ae534a9b2924
Danya <den.martsinovich@yandex.ru>
2019-01-16 01:08:10 +0300

    life on tor1

--- life_on_tor.py
+++ life_on_tor.py
@@ -8,6 +8,7 @@ class Life(Board):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.population = None
+
     def fix(self):
         self.population = np.array(self.board, dtype=np.uint8)
 
