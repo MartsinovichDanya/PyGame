de53917bc911c937ac3ddb2fce152dd40f1feba2
Danya Martsinovich <den.martsinoivch@yandex.ru>
2019-01-16 18:51:24 +0300

    cursor

---
+++ my_cursor.py
@@ -0,0 +1,44 @@
+import pygame
+import os
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+pygame.init()
+uinput = [int(el) for el in input().split()]
+size = width, height = uinput[0], uinput[1]
+screen = pygame.display.set_mode(size)
+screen.fill((0, 0, 0))
+pygame.mouse.set_visible(False)
+all_sprites = pygame.sprite.Group()
+sprite = pygame.sprite.Sprite()
+sprite.image = load_image("cursor.png")
+sprite.rect = sprite.image.get_rect()
+all_sprites.add(sprite)
+
+
+while True:
+    event = pygame.event.wait()
+    if event.type == pygame.QUIT:
+        break
+    if event.type == pygame.MOUSEMOTION:
+        if pygame.mouse.get_focused():
+            sprite.rect.x = event.pos[0]
+            sprite.rect.y = event.pos[1]
+        screen.fill((0, 0, 0))
+        all_sprites.draw(screen)
+    pygame.display.flip()
+pygame.quit()
--- sprites_test.py
+++ sprites_test.py
@@ -10,31 +10,45 @@ def load_image(name, colorkey=None):
     except pygame.error as message:
         print('Cannot load image:', name)
         raise SystemExit(message)
+    image = image.convert_alpha()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
 
-
-all_sprites = pygame.sprite.Group()
-sprite = pygame.sprite.Sprite()
-sprite.image = load_image("bomb.png")
-sprite.rect = sprite.image.get_rect()
-all_sprites.add(sprite)
-sprite.rect.x = 5
-sprite.rect.y = 20
-bomb_image = load_image("bomb.png")
 
 pygame.init()
 uinput = [int(el) for el in input().split()]
 size = width, height = uinput[0], uinput[1]
-for i in range(50):
-    bomb = pygame.sprite.Sprite(all_sprites)
-    bomb.image = bomb_image
-    bomb.rect = bomb.image.get_rect()
-    bomb.rect.x = random.randrange(width)
-    bomb.rect.y = random.randrange(height)
 screen = pygame.display.set_mode(size)
+all_sprites = pygame.sprite.Group()
+
+
+class Bomb(pygame.sprite.Sprite):
+    image = load_image("bomb.png")
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = Bomb.image
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(width)
+        self.rect.y = random.randrange(height)
+
+    def update(self):
+        self.rect = self.rect.move(random.randrange(3) - 1,
+                                   random.randrange(3) - 1)
+
+
+for _ in range(50):
+    Bomb(all_sprites)
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill((0, 0, 0))
     all_sprites.draw(screen)
+    all_sprites.update()
     pygame.display.flip()
